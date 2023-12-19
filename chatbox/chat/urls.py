1331f2dab77c1fd3ba6ddf60e5b6aff17d59ba58
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 13:13:49 +0200

    updated urls for join and view

--- chatbox/chat/urls.py
+++ chatbox/chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_list, conversation_list, message_list, home
+from .views import user_list, conversation_list, message_list, home, join_conversation, view_messages
 
 urlpatterns = [
     path('', home, name='home'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('conversations/', conversation_list, name='conversation_list'),
     path('user/<int:user_id>/conversations/', conversation_list, name='conversation_list'),
     path('conversation/<int:conversation_id>/', message_list, name='message_list'),
+    path('join/', join_conversation, name='join_conversation'),
+    path('view_messages/<int:conversation_id>/', view_messages, name='view_messages'),
 ]
