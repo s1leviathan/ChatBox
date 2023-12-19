cfb50554809863255d20c22656fd584f6d967f46
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 11:03:08 +0200

    messages-view

--- chatbox/chat/views.py
+++ chatbox/chat/views.py
@@ -8,3 +8,8 @@ def user_list(request):
 def conversation_list(request):
     conversations = Conversation.objects.all()
     return render(request, 'chat/conversation_list.html', {'conversations': conversations})
+
+def message_list(request, conversation_id):
+    conversation = Conversation.objects.get(pk=conversation_id)
+    messages = Messages.objects.filter(conversation=conversation)
+    return render(request, 'chat/message_list.html', {'messages': messages, 'conversation': conversation})
