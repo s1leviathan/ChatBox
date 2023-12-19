334d7ff5b54c9bf9cfa21baad64a6cd84c687681
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 10:54:43 +0200

    model-users

--- chatbox/chat/models.py
+++ chatbox/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
