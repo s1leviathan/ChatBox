4f83d66691b4116eb354e42864ca602a282e3b09
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 11:14:23 +0200

    conv-admin

--- chatbox/chat/admin.py
+++ chatbox/chat/admin.py
@@ -4,3 +4,12 @@ from .models import Users, Conversation, Messages
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email')
     search_fields = ('name', 'email')
+
+
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'display_users', 'date')
+
+    def display_users(self, obj):
+        return ', '.join([user.name for user in obj.users.all()])
+
+    display_users.short_description = 'Users'
