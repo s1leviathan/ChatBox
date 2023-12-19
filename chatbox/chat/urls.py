a27bb0dc272b3db2735bd4f5f6f81637bbe8811b
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 11:46:03 +0200

    inserted url

--- chatbox/chat/urls.py
+++ chatbox/chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import user_list, conversation_list, message_list
+from .views import user_list, conversation_list, message_list, home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('users/', user_list, name='user_list'),
     path('conversations/', conversation_list, name='conversation_list'),
     path('conversation/<int:conversation_id>/', message_list, name='message_list'),
