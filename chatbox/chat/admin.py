797528072a0da879b7d3f29fd2f0b2dbdbdb6273
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 11:13:54 +0200

    users-admin

--- chatbox/chat/admin.py
+++ chatbox/chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Users, Conversation, Messages
 
-# Register your models here.
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email')
+    search_fields = ('name', 'email')
