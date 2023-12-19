2f8ede51b49b46bb3336bd7e077fc3ef01b01597
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 10:57:24 +0200

    conv-model

--- chatbox/chat/models.py
+++ chatbox/chat/models.py
@@ -9,3 +9,10 @@ class Users(models.Model):
     def __str__(self):
         return self.name
 
+class Conversation(models.Model):
+    id = models.AutoField(primary_key=True)
+    users = models.ManyToManyField(Users)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id}"
