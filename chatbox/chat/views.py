fa6ca56a83477409082e4c3559664989c519f004
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 11:02:22 +0200

    conv-view

--- chatbox/chat/views.py
+++ chatbox/chat/views.py
@@ -4,3 +4,7 @@ from .models import Users, Conversation, Messages
 def user_list(request):
     users = Users.objects.all()
     return render(request, 'chat/user_list.html', {'users': users})
+
+def conversation_list(request):
+    conversations = Conversation.objects.all()
+    return render(request, 'chat/conversation_list.html', {'conversations': conversations})
