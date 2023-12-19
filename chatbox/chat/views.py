c50a36624b416f7f5b5e796261b649c16da119df
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 12:08:35 +0200

    reply

--- chatbox/chat/views.py
+++ chatbox/chat/views.py
@@ -1,4 +1,6 @@
+import time
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Users, Conversation, Messages
 
 def home(request):
@@ -23,4 +25,20 @@ def conversation_list(request):
 def message_list(request, conversation_id):
     conversation = Conversation.objects.get(pk=conversation_id)
     messages = Messages.objects.filter(conversation=conversation)
+
+    if request.method == 'POST':
+        time.sleep(1)  
+        user_name = request.POST.get('name', '')
+        user_email = request.POST.get('email', '')
+        text = request.POST.get('message', '')
+
+        if user_name and user_email and text:
+            user, created = Users.objects.get_or_create(email=user_email, defaults={'name': user_name})
+            Messages.objects.create(user=user, conversation=conversation, text=text)
+            bot_user = Users.objects.get(name="d1")
+            auto_reply = "Sending Message, please wait"
+            Messages.objects.create(user=bot_user, conversation=conversation, text=auto_reply)
+
+        return redirect('message_list', conversation_id=conversation_id)
+
     return render(request, 'chat/message_list.html', {'messages': messages, 'conversation': conversation})
