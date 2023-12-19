d10ada56ffb72d651a3b17a43a0f41032698eb41
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-19 11:01:52 +0200

    user-view

--- chatbox/chat/views.py
+++ chatbox/chat/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Users, Conversation, Messages
 
-# Create your views here.
+def user_list(request):
+    users = Users.objects.all()
+    return render(request, 'chat/user_list.html', {'users': users})
