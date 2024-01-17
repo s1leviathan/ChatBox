62b4ff21569131aabaaaa04654f07eea186370a0
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2024-01-17 22:06:39 +0200

    Delete ChatBox/chat directory

--- ChatBox/chat/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class MessageForm(forms.Form):
-    message = forms.CharField(widget=forms.Textarea, required=True)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        return cleaned_data
